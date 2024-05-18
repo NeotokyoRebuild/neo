2fec40c66a6558555a7cd120a82c368a7eb3a652
AdamTadeusz <44210793+AdamTadeusz@users.noreply.github.com>
2024-05-18 11:28:53 +0000

    added end of round image and text (#234)

--- mp/src/game/client/neo/ui/neo_hud_game_event.h
+++ mp/src/game/client/neo/ui/neo_hud_game_event.h
@@ -6,19 +6,20 @@
 
 #include "neo_hud_childelement.h"
 #include "hudelement.h"
+#include "hud_macros.h"
 #include "vgui_controls/Panel.h"
 
 #define NEO_MAX_HUD_GAME_EVENT_MSG_SIZE 32 + 1
 
 class CNEOHud_GameEvent : public CNEOHud_ChildElement, public CHudElement, public vgui::Panel
 {
-	DECLARE_CLASS_SIMPLE(CNEOHud_GameEvent, Panel);
-
+	DECLARE_CLASS_SIMPLE(CNEOHud_GameEvent, vgui::Panel);
 public:
-	CNEOHud_GameEvent(const char *pElementName, vgui::Panel *parent = NULL);
+	CNEOHud_GameEvent(const char *pElementName, vgui::Panel *parent);
 
 	void SetMessage(const char *message, size_t size);
 	void SetMessage(const wchar_t *message, size_t size);
+	void MsgFunc_RoundResult(bf_read& msg);
 
 	virtual void Paint();
 
@@ -29,10 +30,23 @@ protected:
 
 private:
 	vgui::HFont m_hFont;
+	vgui::HTexture jinraiWin, nsfWin, tie;
 
 	int m_iResX, m_iResY;
 
+	char teamWon[64];
+	int timeWon;
+	char message[64 + MAX_PLACE_NAME_LENGTH];
+
+	wchar_t messageWord[64 + MAX_PLACE_NAME_LENGTH];
+	int messageWidth;
+	int messageHeight;
+	int messageLength;
+
 	wchar_t m_pszMessage[NEO_MAX_HUD_GAME_EVENT_MSG_SIZE];
+
+	CPanelAnimationVarAliasType(int, image_y_offset, "image_y_offset", "60", "proportional_ypos");
+	CPanelAnimationVarAliasType(int, text_y_offset, "text_y_offset", "420", "proportional_ypos");
 };
 
 
