cabb3f58beea2a7afdc6bb512b8950b3a5c15878
AdamTadeusz <adam.tomaszewski@hotmail.co.uk>
2024-06-07 08:55:26 +0100

    Give AA13 spread cone similar to supa7

--- mp/src/game/shared/neo/weapons/weapon_aa13.cpp
+++ mp/src/game/shared/neo/weapons/weapon_aa13.cpp
@@ -200,7 +200,7 @@ void CWeaponAA13::PrimaryAttack(void)
 	Vector vecSrc = pPlayer->Weapon_ShootPosition();
 	Vector vecAiming = pPlayer->GetAutoaimVector(AUTOAIM_10DEGREES);
 
-	FireBulletsInfo_t info(7, vecSrc, vecAiming, GetBulletSpread(), MAX_TRACE_LENGTH, m_iPrimaryAmmoType);
+	FireBulletsInfo_t info(5, vecSrc, vecAiming, VECTOR_CONE_7DEGREES, MAX_TRACE_LENGTH, m_iPrimaryAmmoType);
 	info.m_pAttacker = pPlayer;
 
 	// Fire the bullets, and force the first shot to be perfectly accurate
