0e260708260ed3618c82f16d10469c2598a2650a
nullsystem <15316579+nullsystem@users.noreply.github.com>
2024-06-13 01:19:11 +0100

    Enforce git, not optional

--- mp/src/cmake/build_info.cmake
+++ mp/src/cmake/build_info.cmake
@@ -1,25 +1,22 @@
-set(GIT_HASH "unknown")
 string(TIMESTAMP BUILD_DATE "%Y%m%d")
 string(TIMESTAMP BUILD_DATETIME UTC)
 
-find_package(Git QUIET)
-if (GIT_FOUND)
-    execute_process(
-        COMMAND git log -1 --format=%h
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE GIT_HASH
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
-    )
-    execute_process(
-        COMMAND git log -1 --format=%H
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        OUTPUT_VARIABLE GIT_LONGHASH
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
-    )
-    string(SUBSTRING "${GIT_HASH}" 0 7 GIT_HASH)
-endif()
+find_package(Git REQUIRED)
+execute_process(
+    COMMAND git log -1 --format=%h
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_HASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET
+)
+execute_process(
+    COMMAND git log -1 --format=%H
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    OUTPUT_VARIABLE GIT_LONGHASH
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_QUIET
+)
+string(SUBSTRING "${GIT_HASH}" 0 7 GIT_HASH)
 
 configure_file(
     ${CMAKE_SOURCE_DIR}/../game/neo/resource/GameMenu.preproc.res
