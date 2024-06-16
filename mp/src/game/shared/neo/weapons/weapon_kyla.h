786ed4be1e7404c50c3e72135e0554131d7feda3
nullsystem <15316579+nullsystem@users.noreply.github.com>
2024-06-16 14:13:26 +0100

    Weapon firerates - Make it use the script instead and parity to OG:NT
    * Instead of fixed in weapons header files, read from the scripts instead
    * Also fix the weapons to OG:NT script values, apart from knife
    * fixes #323 fixes #115

--- mp/src/game/shared/neo/weapons/weapon_kyla.h
+++ mp/src/game/shared/neo/weapons/weapon_kyla.h
@@ -33,7 +33,6 @@ public:
 
 	virtual float GetSpeedScale(void) const { return 1.0; }
 
-	virtual float GetFireRate(void) OVERRIDE { return 0.35f; }
 protected:
 	virtual float GetFastestDryRefireTime() const OVERRIDE { return 0.2f; }
 	virtual float GetAccuracyPenalty() const OVERRIDE { return 0.2f; }
