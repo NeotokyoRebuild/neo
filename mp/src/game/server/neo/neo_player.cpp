4fe4bd8af366836b3148665b269468534d7b747b
Rain <Rainyan@users.noreply.github.com>
2024-06-24 21:55:40 +0300

    Refactor

--- mp/src/game/server/neo/neo_player.cpp
+++ mp/src/game/server/neo/neo_player.cpp
@@ -623,33 +623,34 @@ void CNEO_Player::CheckVisionButtons()
 
 void CNEO_Player::CheckLeanButtons()
 {
-	if (IsAlive())
+	if (!IsAlive())
 	{
-		if (ClientWantsLeanToggle(this))
+		return;
+	}
+
+	if (ClientWantsLeanToggle(this))
+	{
+		if (m_afButtonPressed & IN_LEAN_LEFT)
 		{
-			if (m_afButtonPressed & IN_LEAN_LEFT)
-			{
-				if (m_bInLean == NEO_LEAN_LEFT) m_bInLean = NEO_LEAN_NONE;
-				else m_bInLean = NEO_LEAN_LEFT;
-			}
-			if (m_afButtonPressed & IN_LEAN_RIGHT)
-			{
-				if (m_bInLean == NEO_LEAN_RIGHT) m_bInLean = NEO_LEAN_NONE;
-				else m_bInLean = NEO_LEAN_RIGHT;
-			}
+			if (m_bInLean == NEO_LEAN_LEFT) m_bInLean = NEO_LEAN_NONE;
+			else m_bInLean = NEO_LEAN_LEFT;
 		}
-		else
+		if (m_afButtonPressed & IN_LEAN_RIGHT)
 		{
-			m_bInLean = NEO_LEAN_NONE;
-			if ((m_nButtons & IN_LEAN_LEFT) && !(m_nButtons & IN_LEAN_RIGHT))
-			{
-				m_bInLean = NEO_LEAN_LEFT;
-			}
-			else if ((m_nButtons & IN_LEAN_RIGHT) && !(m_nButtons & IN_LEAN_LEFT))
-			{
-				m_bInLean = NEO_LEAN_RIGHT;
-			}
+			if (m_bInLean == NEO_LEAN_RIGHT) m_bInLean = NEO_LEAN_NONE;
+			else m_bInLean = NEO_LEAN_RIGHT;
 		}
+		return;
+	}
+	
+	m_bInLean = NEO_LEAN_NONE;
+	if ((m_nButtons & IN_LEAN_LEFT) && !(m_nButtons & IN_LEAN_RIGHT))
+	{
+		m_bInLean = NEO_LEAN_LEFT;
+	}
+	else if ((m_nButtons & IN_LEAN_RIGHT) && !(m_nButtons & IN_LEAN_LEFT))
+	{
+		m_bInLean = NEO_LEAN_RIGHT;
 	}
 }
 
