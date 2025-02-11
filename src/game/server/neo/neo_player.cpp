dc1c98ffd222e227df88b50accfc1cd4bf5e982e
AdamTadeusz <adam.tomaszewski@hotmail.co.uk>
2025-02-11 21:07:27 +0000

    default to TEAM_UNASSIGNED

--- src/game/server/neo/neo_player.cpp
+++ src/game/server/neo/neo_player.cpp
@@ -607,12 +607,13 @@ void CNEO_Player::Spawn(void)
 	if (teamNumber == TEAM_UNASSIGNED && gpGlobals->eLoadType != MapLoad_Background)
 	{
 		char commandBuffer[11];
-		if (NEORules()->GetForcedTeam() < 0)
+		int forcedTeam = NEORules()->GetForcedTeam();
+		if (NEORules()->GetForcedTeam() < 1) // don't let this loop infinitely if forcedTeam set to TEAM_UNASSIGNED
 		{
 			engine->ClientCommand(this->edict(), "teammenu");
 			return;
 		}
-		ChangeTeam(NEORules()->GetForcedTeam());
+		ChangeTeam(forcedTeam);
 
 		if (NEORules()->GetForcedClass() < 0)
 		{
@@ -2504,40 +2505,7 @@ void CNEO_Player::PickDefaultSpawnTeam(void)
 {
 	if (!GetTeamNumber())
 	{
-		if (!NEORules()->IsTeamplay())
-		{
-			ProcessTeamSwitchRequest(TEAM_SPECTATOR);
-		}
-		else
-		{
-			ProcessTeamSwitchRequest(TEAM_SPECTATOR);
-
-#if(0) // code for random team selection
-			CTeam *pJinrai = g_Teams[TEAM_JINRAI];
-			CTeam *pNSF = g_Teams[TEAM_NSF];
-
-			if (!pJinrai || !pNSF)
-			{
-				ProcessTeamSwitchRequest(random->RandomInt(TEAM_JINRAI, TEAM_NSF));
-			}
-			else
-			{
-				if (pJinrai->GetNumPlayers() > pNSF->GetNumPlayers())
-				{
-					ProcessTeamSwitchRequest(TEAM_NSF);
-				}
-				else if (pNSF->GetNumPlayers() > pJinrai->GetNumPlayers())
-				{
-					ProcessTeamSwitchRequest(TEAM_JINRAI);
-				}
-				else
-				{
-					ProcessTeamSwitchRequest(random->RandomInt(TEAM_JINRAI, TEAM_NSF));
-				}
-			}
-#endif
-		}
-
+		ProcessTeamSwitchRequest(TEAM_UNASSIGNED);
 		if (!GetModelPtr())
 		{
 			SetPlayerTeamModel();
@@ -2547,7 +2515,7 @@ void CNEO_Player::PickDefaultSpawnTeam(void)
 
 bool CNEO_Player::ProcessTeamSwitchRequest(int iTeam)
 {
-	if (!GetGlobalTeam(iTeam) || iTeam == 0)
+	if (!GetGlobalTeam(iTeam))
 	{
 		Warning("HandleCommand_JoinTeam( %d ) - invalid team index.\n", iTeam);
 		return false;
@@ -2614,7 +2582,7 @@ bool CNEO_Player::ProcessTeamSwitchRequest(int iTeam)
 	}
 
 	const bool suicidePlayerIfAlive = sv_neo_change_suicide_player.GetBool();
-	if (iTeam == TEAM_SPECTATOR)
+	if (iTeam == TEAM_SPECTATOR || iTeam == TEAM_UNASSIGNED)
 	{
 		if (!mp_allowspectators.GetInt())
 		{
