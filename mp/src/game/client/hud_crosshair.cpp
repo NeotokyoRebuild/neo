b56aff43c937b9c76dbbf06cc3594c248e55e89f
AdamTadeusz <adam.tomaszewski@hotmail.co.uk>
2025-02-19 12:54:51 +0000

    same thing to ShouldDraw so crosshair doesn't flicker

--- mp/src/game/client/hud_crosshair.cpp
+++ mp/src/game/client/hud_crosshair.cpp
@@ -131,7 +131,11 @@ bool CHudCrosshair::ShouldDraw( void )
 	if ( m_bHideCrosshair )
 		return false;
 
+#ifdef NEO
+	C_BasePlayer* pPlayer = IsLocalPlayerSpectator() ? UTIL_PlayerByIndex(GetSpectatorTarget()) : C_BasePlayer::GetLocalPlayer();
+#else
 	C_BasePlayer* pPlayer = C_BasePlayer::GetLocalPlayer();
+#endif // NEO
 	if ( !pPlayer )
 		return false;
 
