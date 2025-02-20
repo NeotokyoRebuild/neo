2876f05cb33e3c02c2bb5906df158bf452388037
AdamTadeusz <adam.tomaszewski@hotmail.co.uk>
2025-02-20 18:18:28 +0000

    displays health as hitpoints

--- src/game/client/neo/c_neo_player.h
+++ src/game/client/neo/c_neo_player.h
@@ -136,6 +136,7 @@ public:
 
 	int GetClass() const { return m_iNeoClass; }
 	int GetStar() const { return m_iNeoStar; }
+	int GetDisplayedHealth(bool asPercent) const;
 
 	bool IsCarryingGhost(void) const;
 
