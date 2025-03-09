49bbb0d4576dc08cc3d2a69e0136c1f9b1c53ac7
AdamTadeusz <adam.tomaszewski@hotmail.co.uk>
2025-03-09 18:51:44 +0000

    unused convar

--- src/game/shared/neo/neo_player_shared.h
+++ src/game/shared/neo/neo_player_shared.h
@@ -231,8 +231,6 @@ extern bool IsThereRoomForLeanSlide(CNEO_Player *player,
 // Is the player allowed to aim zoom with a weapon of this type?
 bool IsAllowedToZoom(CNEOBaseCombatWeapon *pWep);
 
-extern ConVar neo_recon_superjump_intensity;
-
 //ConVar sv_neo_resupply_anywhere("sv_neo_resupply_anywhere", "0", FCVAR_CHEAT | FCVAR_REPLICATED);
 
 inline const char* GetNeoClassName(int neoClassIdx)
