1c47aa8f4258570563dd4fdba6fbf87a141ddddd
Rainyan <Rainyan@users.noreply.github.com>
2025-07-25 11:42:33 +0300

    Add neo_restart_this support (#1216)
    * Add neo_restart_this
    
    Add the neo_restart_this cvar for parity.
    
    This cvar differs from mp_restartgame in that the latter will give a
    countdown whereas neo_restart_this is instant.
    
    * refactor

--- src/game/shared/multiplay_gamerules.cpp
+++ src/game/shared/multiplay_gamerules.cpp
@@ -42,6 +42,10 @@
 	#include "NextBotManager.h"
 #endif
 
+#ifdef NEO
+	#include "neo_gamerules.h"
+#endif
+
 #ifdef TF_DLL
 	#include <unordered_set>
 	#include "hl2orange.spa.h"
@@ -99,6 +103,16 @@ ConVar tv_delaymapchange( "tv_delaymapchange", "0", FCVAR_NONE, "Delays map chan
 ConVar tv_delaymapchange_protect( "tv_delaymapchange_protect", "1", FCVAR_NONE, "Protect against doing a manual map change if HLTV is broadcasting and has not caught up with a major game event such as round_end" );
 
 ConVar mp_restartgame( "mp_restartgame", "0", FCVAR_GAMEDLL, "If non-zero, game will restart in the specified number of seconds" );
+#ifdef NEO
+ConVar neo_restart_this("neo_restart_this", "0", FCVAR_GAMEDLL, "If non-zero, game will restart immediately",
+	[](IConVar* var, const char* pOldValue, float flOldValue)->void {
+		if (ConVarRef(var).GetBool())
+		{
+			if (NEORules()) NEORules()->RestartGame();
+		}
+		var->SetValue("0");
+	});
+#endif
 ConVar mp_restartgame_immediate( "mp_restartgame_immediate", "0", FCVAR_GAMEDLL, "If non-zero, game will restart immediately" );
 
 #ifdef NEO
