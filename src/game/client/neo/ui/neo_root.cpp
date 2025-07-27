f51bb0e43292fe18acbe2e151fa7bfb338a3d135
Rain <Rainyan@users.noreply.github.com>
2025-07-27 15:18:15 +0300

    Fix late-binding UI elements
    The Neo UI elements were only bound once, but if for example the MP3
    menu didn't have a bind when that bound logic ran, it would never get
    associated with a key.
    
    Instead, refresh the binds every time, so that they're always guaranteed
    to be up to date, even if the user re-binds the key after it had been
    bound (or was previously unbound).
    
    We still have the problem here that the user can't assign multiple
    keybinds for the same command, because the latest bind will always take
    precedence.
    
    Possibly(?) related: https://github.com/NeotokyoRebuild/neo/issues/1166

--- src/game/client/neo/ui/neo_root.cpp
+++ src/game/client/neo/ui/neo_root.cpp
@@ -491,14 +491,17 @@ void CNeoRoot::OnRelayedKeyCodeTyped(vgui::KeyCode code)
 		return;
 	}
 
-	if (m_ns.keys.bcConsole <= KEY_NONE)
-	{
-		m_ns.keys.bcConsole = gameuifuncs->GetButtonCodeForBind("neo_toggleconsole");
-		m_ns.keys.bcMP3Player = gameuifuncs->GetButtonCodeForBind("neo_mp3");
-		m_ns.keys.bcTeamMenu = gameuifuncs->GetButtonCodeForBind("teammenu");
-		m_ns.keys.bcClassMenu = gameuifuncs->GetButtonCodeForBind("classmenu");
-		m_ns.keys.bcLoadoutMenu = gameuifuncs->GetButtonCodeForBind("loadoutmenu");
-	}
+	constexpr auto refreshBind = [](ButtonCode_t& mutRefButton, const char* bind)->void{
+		mutRefButton = gameuifuncs->GetButtonCodeForBind(bind);
+	};
+	// Refresh every time, because else if the user unbinds or rebinds the key, it will still incorrectly be mapped there.
+	// NEO FIXME (Rain): We do not currently support binding multiple buttons for the same command;
+	// if the user does: bind a foo; bind b foo; then only the latest bind will work.
+	refreshBind(m_ns.keys.bcConsole, "neo_toggleconsole");
+	refreshBind(m_ns.keys.bcMP3Player, "neo_mp3");
+	refreshBind(m_ns.keys.bcTeamMenu, "teammenu");
+	refreshBind(m_ns.keys.bcClassMenu, "classmenu");
+	refreshBind(m_ns.keys.bcLoadoutMenu, "loadoutmenu");
 
 	if (code == m_ns.keys.bcConsole && code != KEY_BACKQUOTE)
 	{
