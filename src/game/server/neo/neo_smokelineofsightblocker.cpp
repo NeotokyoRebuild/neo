c3422de0f028d21514d0c3d19ee912ffc0d7d21f
Alan Shen <sunzenshen@gmail.com>
2025-07-30 23:40:30 -0600

    Delay activation of smoke LOS blocking

--- src/game/server/neo/neo_smokelineofsightblocker.cpp
+++ src/game/server/neo/neo_smokelineofsightblocker.cpp
@@ -8,3 +8,15 @@ BEGIN_DATADESC(CNEOSmokeLineOfSightBlocker)
     DEFINE_FIELD(m_maxs, FIELD_VECTOR),
 END_DATADESC()
 
+ConVar sv_neo_smoke_blocker_duration("sv_neo_smoke_blocker_duration", "20.5", FCVAR_CHEAT, "Time when LOS block should disappear", true, 0.0, true, 30.0);
+
+// Activation scheduled by CNEOGrenadeSmoke::SetupParticles
+void CNEOSmokeLineOfSightBlocker::ActivateLOSBlocker()
+{
+	// Start blocking LOS
+	Spawn();
+	// Active blocking duration ends after smoke can be seen through by a player
+	float duration = sv_neo_smoke_blocker_duration.GetFloat();
+	SetThink(&CNEOSmokeLineOfSightBlocker::SUB_Remove);
+	SetNextThink(gpGlobals->curtime + duration);
+}
