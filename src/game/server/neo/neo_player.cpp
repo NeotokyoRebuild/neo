51ce7d33b9b0b968bc083cfebd0b1a2084b6e121
Alan Shen <sunzenshen@gmail.com>
2025-10-08 20:56:43 -0600

    Do not allow bot commands in free for all games

--- src/game/server/neo/neo_player.cpp
+++ src/game/server/neo/neo_player.cpp
@@ -3103,6 +3103,12 @@ void CNEO_Player::ToggleBotFollowCommander(CNEO_Player* pCommander)
 		return;
 	}
 
+	if (!NEORules()->IsTeamplay())
+	{
+		// Do not allow bot commanding in free for all game modes
+		return;
+	}
+
 	if (pCommander->GetTeamNumber() != GetTeamNumber())
 	{
 		// Commander is not on the same team, do not allow toggling follow state.
