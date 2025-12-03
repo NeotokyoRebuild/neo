a3a3becb596c0497ee28811bb9d7755152d3ce92
Rain <Rainyan@users.noreply.github.com>
2025-12-04 00:02:04 +0200

    Improve narrow_cast

--- src/common/neo/narrow_cast.h
+++ src/common/neo/narrow_cast.h
@@ -27,25 +27,31 @@ concept DifferentTypes = (!std::is_same_v<T, U>);
 // Asserts that value v must fit into type <Container>.
 template <Arithmetic Container, Arithmetic Value>
 	requires DifferentTypes<Container, Value>
-constexpr void AssertFitsInto(Value&& v)
+constexpr void AssertFitsInto(const Value& input)
 {
-	[[maybe_unused]] const auto& input = std::forward<std::remove_reference_t<decltype(v)>>(v);
-	[[maybe_unused]] const auto& output = static_cast<std::add_const_t<Container>&>(input);
-	[[maybe_unused]] const auto& roundtrip = static_cast<decltype(input)>(output);
+	using inputType = std::remove_reference_t<decltype(input)>;
+	using outputType = std::add_const_t<Container>;
+	static_assert(std::is_same_v<decltype(input), inputType&>);
+	static_assert(!std::is_reference_v<inputType>);
+	static_assert(!std::is_reference_v<outputType>);
+
+	[[maybe_unused]] const auto& output = static_cast<outputType&>(input);
 	static_assert(!std::is_same_v<decltype(input), decltype(output)>);
+
+	[[maybe_unused]] const auto& roundtrip = static_cast<decltype(input)>(output);
 	static_assert(std::is_same_v<decltype(input), decltype(roundtrip)>);
 
 	// Value of type A converted to type B, and then back to type A, should equal the original value.
 	Assert(roundtrip == input);
 
 	// Value of type A converted to type B, where the signedness of A and B differs, should not underflow.
-	if (std::is_signed_v<decltype(input)> != std::is_signed_v<decltype(output)>)
+	if (std::is_signed_v<inputType> != std::is_signed_v<outputType>)
 	{
-		Assert(input < decltype(input){} == output < decltype(output){});
+		Assert((input < inputType{}) == (output < outputType{}));
 	}
 }
 #else
-constexpr void AssertFitsInto(auto&&) { /* optimized away */ }
+constexpr void AssertFitsInto(const auto&) { /* optimized away */ }
 #endif // PARANOID_NARROWING
 
 #ifdef PARANOID_NARROWING
@@ -55,7 +61,7 @@ template <Arithmetic Container, Arithmetic Value>
 	requires DifferentTypes<Container, Value>
 constexpr Container narrow_cast(Value&& v)
 {
-	AssertFitsInto<Container>(std::as_const(v));
+	AssertFitsInto<Container>(v);
 	return static_cast<Container>(std::forward<Value>(v));
 }
 #else
