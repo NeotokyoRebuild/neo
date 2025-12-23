46c18279c18706bf204440a7c41570160d45c1f4
Rain <Rainyan@users.noreply.github.com>
2025-12-23 20:37:11 +0200

    Avoid dangling pointer from reference
    The memory is technically properly allocated in the reentrant
    pReturn->ConvertToCopiedData() call in the vscript binding macros,
    but as the reference goes out of scope it could technically blow up.
    
    This change makes g++-14 (SteamRT 14.2.0-19~steamrt3.1+bsrt3.1) 14.2.0
    happy for the release build with -Wall

--- src/public/vscript/variant.h
+++ src/public/vscript/variant.h
@@ -140,11 +140,19 @@ public:
 	void operator=( uint64 i ) 				{ Free(); m_type = FIELD_UINT64; m_uint64 = i; }
 	void operator=( float f ) 				{ Free(); m_type = FIELD_FLOAT; m_float = f; }
 	void operator=( float64 f ) 			{ Free(); m_type = FIELD_FLOAT64; m_float64 = f; }
+#ifdef NEO
+	void operator=( const Vector2D &vec )	{ CopyData( vec, true ); }
+	void operator=( const Vector &vec )		{ CopyData( vec, true ); }
+//	void operator=( const Vector4D &vec )	{ CopyData( vec, true ); }
+	void operator=( const QAngle &vec )		{ CopyData( vec, true ); }
+	void operator=( const Quaternion &q )	{ CopyData( q, true ); }
+#else
 	void operator=( const Vector2D &vec )	{ CopyData( vec ); }
 	void operator=( const Vector &vec )		{ CopyData( vec ); }
 //	void operator=( const Vector4D &vec )	{ CopyData( vec ); }
 	void operator=( const QAngle &vec )		{ CopyData( vec ); }
 	void operator=( const Quaternion &q )	{ CopyData( q ); }
+#endif
 	void operator=( const Vector2D *vec )	{ CopyData( *vec ); }
 	void operator=( QAngle *a )				{ CopyData( *a ); }
 	void operator=( const QAngle *a )		{ CopyData( *a ); }
