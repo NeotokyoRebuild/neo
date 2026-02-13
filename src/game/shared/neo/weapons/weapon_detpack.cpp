NeotokyoRebuild/neo

commit 6ce1212c7cb5bfafce741757e417f6545e1d526b

Prevent detpack trigger cancel
This is parity

src/game/shared/neo/weapons/weapon_detpack.cpp
@@ -109,6 +109,11 @@ bool CWeaponDetpack::Deploy(void)
 // Output : Returns true on success, false on failure.
 bool CWeaponDetpack::Holster(CBaseCombatWeapon* pSwitchingTo)
 {
+	// Cannot put away the det once the trigger is pulled (parity).
+	if (m_bRemoteHasBeenTriggered)
+	{
+		return false;
+	}
 
 	m_fDrawbackFinished = false;
 

commit 4fd04d7555563d8da263649940d5a032cb44864a

Fix broken weapon switch after detpack explosion
Regression from 6ce1212c

src/game/shared/neo/weapons/weapon_detpack.cpp
@@ -131,6 +131,11 @@ bool CWeaponDetpack::Holster(CBaseCombatWeapon* pSwitchingTo)
 	return res;
 }
 
+int CWeaponDetpack::GetWeaponFlags() const
+{
+	return ITEM_FLAG_EXHAUSTIBLE | BaseClass::GetWeaponFlags();
+}
+
 void CWeaponDetpack::PrimaryAttack(void)
 {
 	if (ShootingIsPrevented())
@@ -207,6 +212,13 @@ void CWeaponDetpack::ItemPostFrame(void)
 				g_EventQueue.AddEvent(m_pDetpack, "RemoteDetonate", 0, GetOwner(), GetOwner());
 				// m_pDetpack->Detonate();
 				m_pDetpack = NULL;
+
+				// Need to reset this to pass the Holster check called via SwitchToNextBestWeapon
+				m_bRemoteHasBeenTriggered = false;
+				if (pOwner)
+				{
+					pOwner->SwitchToNextBestWeapon(this);
+				}
 			}
 			else
 			{

src/game/shared/neo/weapons/weapon_detpack.h
@@ -44,6 +44,7 @@ public:
 
 	bool	Deploy(void);
 	virtual bool	Holster(CBaseCombatWeapon* pSwitchingTo = NULL) OVERRIDE;
+	virtual int GetWeaponFlags() const override;
 
 	virtual float GetFastestDryRefireTime() const { return 1.f; } // is called if attack button spammed, doesn't really mean much for grenades
 	const char* GetWorldModel(void) const override { return	m_bThisDetpackHasBeenThrown ? "models/weapons/w_detremote.mdl" : "models/weapons/w_detpack.mdl"; };