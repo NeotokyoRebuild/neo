NeotokyoRebuild/neo

commit 04de06a9f5d27f4a6fff7d72af100db600957527

Use software cursor by default (#1704)
* Use software cursor by default

* Toggle software cursor during loading

Render thread becomes blocked during loading screen, which freezes the software cursor. So always use the OS cursor there.

* Add main menu option for software cursor

* Add comments

* Fix cursor disappearing against empty HUD element background

* Fix software cursor not initializing at UI init

* Fix sw cursor render for net_graph and cl_showpos

src/game/client/game_controls/baseviewport.cpp
@@ -223,7 +223,12 @@ void CBaseViewport::OnScreenSizeChanged(int iOldWide, int iOldTall)
 #ifndef _XBOX
 	m_pBackGround = new CBackGroundPanel( NULL );
 	m_pBackGround->SetZPos( -20 ); // send it to the back 
+#ifdef NEO
+	// The software cursor requires a visible background surface to draw against, even if it's empty.
+	m_pBackGround->SetVisible( true );
+#else
 	m_pBackGround->SetVisible( false );
+#endif
 #endif
 	CreateDefaultPanels();
 #ifndef _XBOX

src/game/client/cdll_client_int.cpp
@@ -1483,6 +1483,13 @@ void CHLClient::PostInit()
 
 			if (iCfgVerMajor < 26)
 			{
+				ConVarRef cl_software_cursor( "cl_software_cursor" );
+				Assert(cl_software_cursor.IsValid());
+				if (cl_software_cursor.IsValid())
+				{
+					cl_software_cursor.SetValue(true);
+				}
+
 				// voice_modenable is used now instead of voice_enable as
 				// that's how the valve settings menu does it now and
 				// force voice_enable back on

src/game/client/hl2mp/clientmode_hl2mpnormal.cpp
@@ -121,6 +121,21 @@ ClientModeHL2MPNormal::ClientModeHL2MPNormal()
 {
 	m_pViewport = new CHudViewport();
 	m_pViewport->Start(gameuifuncs, gameeventmanager);
+#ifdef NEO
+	ConVarRef cl_software_cursor( "cl_software_cursor" );
+	Assert(cl_software_cursor.IsValid());
+	if (cl_software_cursor.IsValid())
+	{
+		if (auto* surface = vgui::surface())
+		{
+			surface->SetSoftwareCursor(cl_software_cursor.GetBool());
+		}
+		else
+		{
+			Assert(false);
+		}
+	}
+#endif
 }
 
 

src/game/client/neo/ui/neo_loading.cpp
@@ -87,10 +87,18 @@ void CNeoLoading::OnMessage(const KeyValues *params, vgui::VPANEL fromPanel)
 	{
 		FetchGameUIPanels();
 		g_pNeoRoot->m_bOnLoadingScreen = true;
+		// The loading screen blocks software cursor render updates, so always use OS cursor for better responsiveness here.
+		vgui::surface()->SetSoftwareCursor(false);
 	}
 	else if (V_strcmp(pSzMsgName, "deactivate") == 0)
 	{
 		g_pNeoRoot->m_bOnLoadingScreen = false;
+		static ConVarRef cl_software_cursor( "cl_software_cursor" );
+
+		// Revert the software cursor option back to user preference once we exit the loading screen.
+		Assert(cl_software_cursor.IsValid());
+		vgui::surface()->SetSoftwareCursor(cl_software_cursor.GetBool());
+
 		if (engine->IsConnected() && !engine->IsLevelMainMenuBackground())
 		{
 			g_pNeoRoot->m_flTimeLoadingScreenTransition -= (NEO_MENU_SECONDS_DELAY + NEO_MENU_SECONDS_TILL_FULLY_OPAQUE); // Don't fade in the menu on disconnect

src/game/client/neo/ui/neo_root_settings.cpp
@@ -610,6 +610,7 @@ void NeoSettingsRestore(NeoSettings *ns, const NeoSettings::Keys::Flags flagsKey
 		pVideo->flGamma = cvr->mat_monitorgamma.GetFloat();
 		pVideo->iFov = cvr->neo_fov.GetInt();
 		pVideo->iViewmodelFov = cvr->neo_viewmodel_fov_offset.GetInt();
+		pVideo->bSoftwareCursor = cvr->cl_software_cursor.GetBool();
 	}
 	{
 		NeoSettings::Crosshair *pCrosshair = &ns->crosshair;
@@ -867,6 +868,7 @@ void NeoSettingsSave(const NeoSettings *ns)
 		cvr->mat_monitorgamma.SetValue(pVideo->flGamma);
 		cvr->neo_fov.SetValue(pVideo->iFov);
 		cvr->neo_viewmodel_fov_offset.SetValue(pVideo->iViewmodelFov);
+		cvr->cl_software_cursor.SetValue(pVideo->bSoftwareCursor);
 	}
 	{
 		const NeoSettings::Crosshair *pCrosshair = &ns->crosshair;
@@ -1227,6 +1229,7 @@ void NeoSettings_Video(NeoSettings *ns)
 	NeoUI::Slider(L"Gamma", &pVideo->flGamma, 1.6, 2.6, 2, 0.1f);
 	NeoUI::SliderInt(L"FOV", &pVideo->iFov, MIN_FOV, MAX_FOV);
 	NeoUI::SliderInt(L"Viewmodel FOV Offset", &pVideo->iViewmodelFov, -20, 40);
+	NeoUI::RingBoxBool(L"Software Cursor", &pVideo->bSoftwareCursor);
 
 	NeoUI::Divider(L"VISUALS");
 	NeoUI::RingBox(L"Model detail", QUALITY_LABELS, 3, &pVideo->iModelDetail);

src/game/client/neo/ui/neo_root_settings.h
@@ -159,6 +159,7 @@ struct NeoSettings
 		float flGamma;
 		int iFov;
 		int iViewmodelFov;
+		bool bSoftwareCursor;
 
 		// Video modes
 		int iVMListSize;
@@ -243,6 +244,7 @@ struct NeoSettings
 		CONVARREF_DEF(cl_onlysteamnick);
 		CONVARREF_DEF(neo_fov);
 		CONVARREF_DEF(neo_viewmodel_fov_offset);
+		CONVARREF_DEF(cl_software_cursor);
 		CONVARREF_DEF(cl_autoreload_when_empty);
 		CONVARREF_DEF(cl_righthand);
 		CONVARREF_DEF(cl_neo_lean_viewmodel_only);

src/game/client/vgui_fpspanel.cpp
@@ -100,6 +100,13 @@ CFPSPanel::CFPSPanel( vgui::VPANEL parent ) : BaseClass( NULL, "CFPSPanel" )
 
 	vgui::ivgui()->AddTickSignal( GetVPanel(), 250 );
 	m_bLastDraw = false;
+
+#ifdef NEO
+	// Software cursor support
+	SetCursor(vgui::dc_arrow);
+#else
+	SetCursor( null );
+#endif
 }
 
 //-----------------------------------------------------------------------------

src/game/client/vgui_netgraphpanel.cpp
@@ -216,7 +216,12 @@ CNetGraphPanel::CNetGraphPanel( VPANEL parent )
 	SetSize( w, h );
 	SetPos( 0, 0 );
 	SetVisible( false );
+#ifdef NEO
+	// Software cursor support
+	SetCursor(vgui::dc_arrow);
+#else
 	SetCursor( null );
+#endif
 
 	m_hFont = 0;
 	m_hFontProportional = 0;

commit da6aa52f6215be9b3b86566b9e8df1ca12f092a2

Fix screen size change breaking brightness
Remove the unneeded line making the background panel visible upon screen
change, because it was causing screen brightness settings to break.

Regression from 04de06a9f5d

src/game/client/game_controls/baseviewport.cpp
@@ -223,12 +223,7 @@ void CBaseViewport::OnScreenSizeChanged(int iOldWide, int iOldTall)
 #ifndef _XBOX
 	m_pBackGround = new CBackGroundPanel( NULL );
 	m_pBackGround->SetZPos( -20 ); // send it to the back 
-#ifdef NEO
-	// The software cursor requires a visible background surface to draw against, even if it's empty.
-	m_pBackGround->SetVisible( true );
-#else
 	m_pBackGround->SetVisible( false );
-#endif
 #endif
 	CreateDefaultPanels();
 #ifndef _XBOX