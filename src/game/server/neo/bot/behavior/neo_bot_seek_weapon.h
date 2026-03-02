517bb3c92fb105ec1687a47c27cc3b9d84aea2b2
Alan Shen <sunzenshen@gmail.com>
2026-03-01 22:58:03 -0700

    Bots swap for more powerful weapons

--- src/game/server/neo/bot/behavior/neo_bot_seek_weapon.h
+++ src/game/server/neo/bot/behavior/neo_bot_seek_weapon.h
@@ -1,20 +1,37 @@
-#ifndef NEO_BOT_SEEK_WEAPON_H
-#define NEO_BOT_SEEK_WEAPON_H
+#pragma once
 
 #include "bot/neo_bot.h"
 
+class CNEOIgnoredWeaponsCache
+{
+public:
+	void Reset( void ) { m_weapons.Purge(); }
+	bool Has( CBaseEntity *pWep ) const { return m_weapons.HasElement( pWep ); }
+	void Add( CBaseEntity *pWep ) { m_weapons.AddToTail( pWep ); }
+
+private:
+	CUtlVector<CHandle<CBaseEntity>> m_weapons;
+};
+
+// Returns the bot's preference rank for a weapon (-1 = not preferred, -2 = empty)
+int GetBotWeaponPreferenceRank( CNEOBot *me, NEO_WEP_BITS_UNDERLYING_TYPE wepBit );
+
+bool IsWeaponPreferenceUpgrade( CNEOBot *me, CNEOBaseCombatWeapon *pTargetWep, int myPrefRank, bool bHasReserveAmmo );
+
+bool IsUndroppablePrimary( CBaseCombatWeapon *pPrimary );
+
 // General utility that can be used to see if it makes sense to transition into this behavior
-CBaseEntity *FindNearestPrimaryWeapon( const Vector &searchOrigin, bool bShortCircuit = false );
+CBaseEntity *FindNearestPrimaryWeapon( CNEOBot *me, bool bAllowDropGhost = false, CNEOIgnoredWeaponsCache *pIgnoredWeapons = nullptr );
 
 //--------------------------------------------------------------------------------------------------------
 class CNEOBotSeekWeapon : public Action< CNEOBot >
 {
 public:
-	CNEOBotSeekWeapon( void );
+	CNEOBotSeekWeapon( CBaseEntity *pTargetWeapon = nullptr, CNEOIgnoredWeaponsCache *pIgnoredWeapons = nullptr );
 
 	virtual ActionResult< CNEOBot > OnStart( CNEOBot *me, Action< CNEOBot > *priorAction ) override;
 	virtual ActionResult< CNEOBot > Update( CNEOBot *me, float interval ) override;
-	virtual ActionResult< CNEOBot > OnResume( CNEOBot *me, Action< CNEOBot > *interruptingAction ) override;
+	virtual ActionResult< CNEOBot >	OnResume( CNEOBot *me, Action< CNEOBot > *priorAction ) override;
 
 	virtual EventDesiredResult< CNEOBot > OnStuck( CNEOBot *me ) override;
 	virtual EventDesiredResult< CNEOBot > OnMoveToSuccess( CNEOBot *me, const Path *path ) override;
@@ -25,8 +42,9 @@ public:
 private:
 	PathFollower m_path;
 	CHandle<CBaseEntity> m_hTargetWeapon;
+	CNEOIgnoredWeaponsCache *m_pIgnoredWeapons;
+	CountdownTimer m_repathTimer;
+	CountdownTimer m_giveUpTimer;
 
 	CBaseEntity *FindAndPathToWeapon( CNEOBot *me );
 };
-
-#endif // NEO_BOT_SEEK_WEAPON_H
