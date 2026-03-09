91db40c94eb180350c2bbd2aeaa7a47f9d6ddb24
Agiel <agiel.negura@gmail.com>
2026-03-09 21:55:42 +0100

    Don't stay scoped while playing bolt animation

--- src/game/shared/neo/neo_player_shared.cpp
+++ src/game/shared/neo/neo_player_shared.cpp
@@ -58,6 +58,10 @@ bool IsAllowedToZoom(CNEOBaseCombatWeapon *pWep)
 		return false;
 	}
 
+	if (pWep->GetNeoWepBits() & NEO_WEP_SRS && pWep->GetIdealActivity() == ACT_VM_PULLBACK) {
+		return false;
+	}
+
 	// These weapons are not allowed to be zoomed in with.
 	const auto forbiddenZooms =
 		NEO_WEP_DETPACK |
