eb8863fa47662177f04d23d1962dff8b4013ddba
Rain <Rainyan@users.noreply.github.com>
2026-03-27 16:04:22 +0200

    Draw at most 1 tracer per shot
    The bullet penetration logic is recursive, and this could cause bullets
    to render multiple tracers per one shot. Check that the current
    penetrating force of the shot equals the used weapon's original
    penetrating force, i.e. that no surfaces have yet been penetrated.
    
    This asserts that any penetrated surfaces will incur a non-zero
    penetration cost.

--- src/game/shared/baseentity_shared.cpp
+++ src/game/shared/baseentity_shared.cpp
@@ -2107,6 +2107,19 @@ void CBaseEntity::FireBullets( const FireBulletsInfo_t &info )
 		}
 #endif // NEO
 
+#ifdef NEO
+		// The below penetration value check relies an all possible surfaces
+		// incurring a non-zero cost to m_flPenetration.
+		for (int i = 0; i < ARRAYSIZE(PENETRATION_RESISTANCE); ++i)
+		{
+			Assert(PENETRATION_RESISTANCE[i] > 0);
+		}
+		// Only draw tracers if we haven't yet penetrated, because the penetration logic is reentrant
+		// and would otherwise draw multiple tracers per shot.
+		Assert(neoWeapon);
+		if (neoWeapon->GetPenetration() == info.m_flPenetration)
+		{
+#endif
 		if ( ( info.m_iTracerFreq != 0 ) && ( tracerCount++ % info.m_iTracerFreq ) == 0 && ( bHitGlass == false ) )
 		{
 			if ( bDoServerEffects == true )
@@ -2150,11 +2163,16 @@ void CBaseEntity::FireBullets( const FireBulletsInfo_t &info )
 				}
 #endif //#ifdef PORTAL
 			}
+#ifndef NEO
 			else
 			{
 				bDoTracers = true;
 			}
+#endif
 		}
+#ifdef NEO
+		}
+#endif
 
 		//NOTENOTE: We could expand this to a more general solution for various material penetration types (wood, thin metal, etc)
 
