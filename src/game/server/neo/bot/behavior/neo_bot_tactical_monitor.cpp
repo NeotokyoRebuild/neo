e4b5397197682536a5525d354c00932f0a88b6c9
Alan Shen <1976941+sunzenshen@users.noreply.github.com>
2026-04-05 12:10:07 -0600

    Bot behavior improvements when destroying breakables in path (#1812)
    - Moved bot breakables path detection routine into tactical monitor
    
    - Consolidate bot breakables path clearing into a dedicated behavior class 
    
    - Behavior tweaks to minimize friendly fire when attacking breakables
    
    - Bots prioritize knife > pistol > primary as the tool for breaking the breakable

--- src/game/server/neo/bot/behavior/neo_bot_tactical_monitor.cpp
+++ src/game/server/neo/bot/behavior/neo_bot_tactical_monitor.cpp
@@ -17,6 +17,7 @@
 #include "bot/behavior/neo_bot_retreat_from_grenade.h"
 #include "bot/behavior/neo_bot_ladder_approach.h"
 #include "bot/behavior/neo_bot_ladder_climb.h"
+#include "bot/behavior/neo_bot_path_clear_breakable.h"
 #include "bot/behavior/neo_bot_pause.h"
 #if 0 // NEO TODO (Adam) Fix picking up weapons, search for dropped weapons to pick up ammo
 #include "bot/behavior/neo_bot_get_ammo.h"
@@ -267,6 +268,11 @@ ActionResult< CNEOBot >	CNEOBotTacticalMonitor::Update( CNEOBot *me, float inter
 				}
 			}
 		}
+	}
+
+	if ( CBaseEntity *breakable = CNEOBotPathClearBreakable::GetBreakableInPath( me ) )
+	{
+		return SuspendFor( new CNEOBotPathClearBreakable( breakable ), "Clearing breakable in path" );
 	}
 
 	ActionResult< CNEOBot > scavengeResult = ScavengeForPrimaryWeapon( me );
