a6ea02ee07be6a95faf0de6c1e12a3b69c0844d7
Rainyan <Rainyan@users.noreply.github.com>
2026-06-03 22:23:39 +0300

    Fix missing script sound entries for VPK build (#1950)
    * Import OGNT soundscript sounds_player entries
    
    This commit is a verbatim copy of the OGNT file.
    
    Added for supporting the move to asset VPK mounting.
    
    * Remove unused HL2MP font reference
    
    Fix error: Failed to load custom font file 'resource/hl2mp.ttf'
    
    This is unrelated to the VPK changes but might as well fix the console
    warning.
    
    * Remove victory jingle entries
    
    Already exists in game_sounds_ui.txt as HUD.JinraiWin, etc.
    Also, we typically call these directly with the sound filename
    because the volume level must correspond to "snd_victory_volume" cvar.
    
    * Remove unused OGNT sounds
    
    No SFX for rank up/down
    
    * Refactor
    
    Use the correct enum def (same value)
    
    * Update g_pChannelNames
    
    * Revert "Remove unused OGNT sounds"
    
    This reverts commit 46a18bc54b1cf876c6cac274519bceb7e3d75f4d.
    
    Revert removal of the unused rankup/rankdown sfx as per Discord discussion.

--- src/game/server/neo/neo_player.cpp
+++ src/game/server/neo/neo_player.cpp
@@ -4007,7 +4007,7 @@ void CNEO_Player::SpawnJuggernautPostDeath()
 		{
 			EmitSound_t soundParams;
 			soundParams.m_pSoundName = "HUD.GhostPickUp";
-			soundParams.m_nChannel = CHAN_USER_BASE;
+			soundParams.m_nChannel = CHAN_GHOST_PICKUP;
 			soundParams.m_bWarnOnDirectWaveReference = false;
 			soundParams.m_bEmitCloseCaption = false;
 			soundParams.m_SoundLevel = ATTN_TO_SNDLVL(ATTN_NONE);
