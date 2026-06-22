c7ba598ecc89bfa167af6e0674c6cd0190595d7f
Masterkatze <masterkatze@gmail.com>
2026-06-22 10:29:15 +0300

    Some cleanup

--- src/utils/vvis/vvis.cpp
+++ src/utils/vvis/vvis.cpp
@@ -981,7 +981,7 @@ int RunVVis( int argc, char **argv )
 	double		start, end;
 
 
-	Msg( "Valve Software - vvis.exe (%s)\n", __DATE__ );
+	Msg( "Valve Software - vvis (%s)\n", __DATE__ );
 
     verbose = false;
 
