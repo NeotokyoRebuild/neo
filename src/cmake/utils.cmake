56957730662a9693e8cc436a04e9dfc36a1d9836
Masterkatze <masterkatze@gmail.com>
2026-08-20 22:57:28 +0300

    Create symlink to ntre dir for tools when path is specified

--- src/cmake/utils.cmake
+++ src/cmake/utils.cmake
@@ -100,6 +100,15 @@ function(add_origin_rpath)
     else()
         return()
     endif()
+    
+    # Create a symlink so libraries (materialsystem.so, shaderapiempty.so, ...)
+    # can be loaded using dlopen from the CMake binary dir
+    if(NEO_TOOLS_STEAM_NTRE_PATH)
+        set(NEO_ENGINE_SYMLINK_NAME "engine")
+        
+        file(CREATE_LINK "${NEO_TOOLS_STEAM_NTRE_PATH}" "${CMAKE_CURRENT_BINARY_DIR}/${NEO_ENGINE_SYMLINK_NAME}" SYMBOLIC)
+        list(APPEND PARSED_ARGS_PATHS "${NEO_ENGINE_SYMLINK_NAME}" "../${NEO_ENGINE_SYMLINK_NAME}")
+    endif()
 
     set(RPATH "${ORIGIN_TOKEN}")
     foreach(RELATIVE_PATH ${PARSED_ARGS_PATHS})
