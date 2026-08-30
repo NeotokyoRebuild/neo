120a77c530c494675eb389d1ec69968377a6da55
nullsystem <15316579+nullsystem@users.noreply.github.com>
2026-08-30 15:08:16 +0100

    Add RequestUserInformation call
    Somehow Windows need it but Linux doesn't? But just one thing that was
    glossed over verses vgui avatar version.

--- src/game/client/neo/ui/neo_avatar.cpp
+++ src/game/client/neo/ui/neo_avatar.cpp
@@ -42,106 +42,111 @@ void NeoAvatar::SetSteamID(const CSteamID &steamID)
 void NeoAvatar::Fetch(const int iAvatarWH)
 {
 	if (!m_SteamID.IsValid()
+			|| !steamapicontext->SteamFriends()
+			|| !steamapicontext->SteamUtils()
 			|| (m_iTextureID > 0 && m_iTexForAvatarWH == iAvatarWH)
 			|| (m_flPrevLoadAttempt + 1.0f) > gpGlobals->realtime)
 	{
 		return;
 	}
 
-	int iAvatar = 0;
-	if (iAvatarWH <= 32)
+	if (!steamapicontext->SteamFriends()->RequestUserInformation(m_SteamID, false))
 	{
-		iAvatar = steamapicontext->SteamFriends()->GetSmallFriendAvatar(m_SteamID);
-	}
-	else if (iAvatarWH <= 64)
-	{
-		iAvatar = steamapicontext->SteamFriends()->GetMediumFriendAvatar(m_SteamID);
-	}
-	else
-	{
-		iAvatar = steamapicontext->SteamFriends()->GetLargeFriendAvatar(m_SteamID);
-	}
-	m_iTexForAvatarWH = iAvatarWH;
-
-	if (iAvatar > 0)
-	{
-		if (!gAvatarImageCacheInit)
+		int iAvatar = 0;
+		if (iAvatarWH <= 32)
 		{
-			SetDefLessFunc(gAvatarImageCache);
-			gAvatarImageCacheInit = true;
+			iAvatar = steamapicontext->SteamFriends()->GetSmallFriendAvatar(m_SteamID);
 		}
-
-		int iTexIndex = gAvatarImageCache.Find(CacheAvatarKey{m_SteamID, iAvatar});
-		if (iTexIndex == gAvatarImageCache.InvalidIndex())
+		else if (iAvatarWH <= 64)
 		{
-			uint32 u32wide = 0, u32tall = 0;
-			if (steamapicontext->SteamUtils()->GetImageSize(iAvatar, &u32wide, &u32tall)
-					&& u32wide > 0 && u32tall > 0)
-			{
-				const int wide = u32wide;
-				const int tall = u32tall;
-				const int destBufferSize = wide * tall * 4;
-				byte *rgbaBuf = (byte *)stackalloc(destBufferSize);
-				if (steamapicontext->SteamUtils()->GetImageRGBA(iAvatar, rgbaBuf, destBufferSize))
-				{
-					// Create normal avatar from RGBA without edits
-					m_iTextureID = vgui::surface()->CreateNewTextureID(true);
-					g_pMatSystemSurface->DrawSetTextureRGBAEx2(m_iTextureID, rgbaBuf, wide, tall, IMAGE_FORMAT_RGBA8888, true);
-
-					// Create dead avatar from RGBA with redness edits
-					for (int offset = 0; offset < (wide * tall * 4); offset += 4)
-					{
-						constexpr float brightness = 0.5f;
-						constexpr float contrast = 1.5f;
-
-						float r = (rgbaBuf + offset)[0] / 255.0f;
-						float g = (rgbaBuf + offset)[1] / 255.0f;
-						float b = (rgbaBuf + offset)[2] / 255.0f;
-
-						// Contrast
-						r = Clamp((r - 0.5f) * contrast + 0.5f, 0.0f, 1.0f);
-						g = Clamp((g - 0.5f) * contrast + 0.5f, 0.0f, 1.0f);
-						b = Clamp((b - 0.5f) * contrast + 0.5f, 0.0f, 1.0f);
-
-						// Convert to grayscale - Luminosity, then gradient from black -> red -> white
-						const float gray = 0.3f * r + 0.59f * g  + 0.11f * b;
-						if (gray < 0.5)
-						{
-							r = gray * 2.0f;
-							g = 0;
-							b = 0;
-						}
-						else {
-							r = 1.0f;
-							g = (gray - 0.5f) * 2.0f;
-							b = (gray - 0.5f) * 2.0f;
-						}
-
-						// Brightness
-						r = Clamp(r * brightness, 0.0f, 1.0f);
-						g = Clamp(g * brightness, 0.0f, 1.0f);
-						b = Clamp(b * brightness, 0.0f, 1.0f);
-
-						(rgbaBuf + offset)[0] = r * 255;
-						(rgbaBuf + offset)[1] = g * 255;
-						(rgbaBuf + offset)[2] = b * 255;
-					}
-
-					m_iTextureDeadID = vgui::surface()->CreateNewTextureID(true);
-					g_pMatSystemSurface->DrawSetTextureRGBAEx2(m_iTextureDeadID, rgbaBuf, wide, tall, IMAGE_FORMAT_RGBA8888, true);
-
-					// Add textures to global cache
-					iTexIndex = gAvatarImageCache.Insert(CacheAvatarKey{m_SteamID, iAvatar});
-					gAvatarImageCache[iTexIndex].normal = m_iTextureID;
-					gAvatarImageCache[iTexIndex].dead = m_iTextureDeadID;
-				}
-				stackfree(rgbaBuf);
-			}
+			iAvatar = steamapicontext->SteamFriends()->GetMediumFriendAvatar(m_SteamID);
 		}
 		else
 		{
-			m_iTextureID = gAvatarImageCache[iTexIndex].normal;
-			m_iTextureDeadID = gAvatarImageCache[iTexIndex].dead;
+			iAvatar = steamapicontext->SteamFriends()->GetLargeFriendAvatar(m_SteamID);
+		}
+		m_iTexForAvatarWH = iAvatarWH;
+
+		if (iAvatar > 0)
+		{
+			if (!gAvatarImageCacheInit)
+			{
+				SetDefLessFunc(gAvatarImageCache);
+				gAvatarImageCacheInit = true;
+			}
+
+			int iTexIndex = gAvatarImageCache.Find(CacheAvatarKey{m_SteamID, iAvatar});
+			if (iTexIndex == gAvatarImageCache.InvalidIndex())
+			{
+				uint32 u32wide = 0, u32tall = 0;
+				if (steamapicontext->SteamUtils()->GetImageSize(iAvatar, &u32wide, &u32tall)
+						&& u32wide > 0 && u32tall > 0)
+				{
+					const int wide = u32wide;
+					const int tall = u32tall;
+					const int destBufferSize = wide * tall * 4;
+					byte *rgbaBuf = (byte *)stackalloc(destBufferSize);
+					if (steamapicontext->SteamUtils()->GetImageRGBA(iAvatar, rgbaBuf, destBufferSize))
+					{
+						// Create normal avatar from RGBA without edits
+						m_iTextureID = vgui::surface()->CreateNewTextureID(true);
+						g_pMatSystemSurface->DrawSetTextureRGBAEx2(m_iTextureID, rgbaBuf, wide, tall, IMAGE_FORMAT_RGBA8888, true);
+
+						// Create dead avatar from RGBA with redness edits
+						for (int offset = 0; offset < (wide * tall * 4); offset += 4)
+						{
+							constexpr float brightness = 0.5f;
+							constexpr float contrast = 1.5f;
+
+							float r = (rgbaBuf + offset)[0] / 255.0f;
+							float g = (rgbaBuf + offset)[1] / 255.0f;
+							float b = (rgbaBuf + offset)[2] / 255.0f;
+
+							// Contrast
+							r = Clamp((r - 0.5f) * contrast + 0.5f, 0.0f, 1.0f);
+							g = Clamp((g - 0.5f) * contrast + 0.5f, 0.0f, 1.0f);
+							b = Clamp((b - 0.5f) * contrast + 0.5f, 0.0f, 1.0f);
+
+							// Convert to grayscale - Luminosity, then gradient from black -> red -> white
+							const float gray = 0.3f * r + 0.59f * g  + 0.11f * b;
+							if (gray < 0.5)
+							{
+								r = gray * 2.0f;
+								g = 0;
+								b = 0;
+							}
+							else {
+								r = 1.0f;
+								g = (gray - 0.5f) * 2.0f;
+								b = (gray - 0.5f) * 2.0f;
+							}
+
+							// Brightness
+							r = Clamp(r * brightness, 0.0f, 1.0f);
+							g = Clamp(g * brightness, 0.0f, 1.0f);
+							b = Clamp(b * brightness, 0.0f, 1.0f);
+
+							(rgbaBuf + offset)[0] = r * 255;
+							(rgbaBuf + offset)[1] = g * 255;
+							(rgbaBuf + offset)[2] = b * 255;
+						}
+
+						m_iTextureDeadID = vgui::surface()->CreateNewTextureID(true);
+						g_pMatSystemSurface->DrawSetTextureRGBAEx2(m_iTextureDeadID, rgbaBuf, wide, tall, IMAGE_FORMAT_RGBA8888, true);
+
+						// Add textures to global cache
+						iTexIndex = gAvatarImageCache.Insert(CacheAvatarKey{m_SteamID, iAvatar});
+						gAvatarImageCache[iTexIndex].normal = m_iTextureID;
+						gAvatarImageCache[iTexIndex].dead = m_iTextureDeadID;
+					}
+					stackfree(rgbaBuf);
+				}
+			}
+			else
+			{
+				m_iTextureID = gAvatarImageCache[iTexIndex].normal;
+				m_iTextureDeadID = gAvatarImageCache[iTexIndex].dead;
+			}
 		}
 	}
 
