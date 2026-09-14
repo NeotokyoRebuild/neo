f58e8062257ff651659bb00a1b0c26effccadf26
Alan Shen <sunzenshen@gmail.com>
2026-09-13 19:56:20 -0600

    Optimize bot grenade emergence point targeting

--- src/game/server/neo/bot/behavior/neo_bot_grenade_throw_smoke.cpp
+++ src/game/server/neo/bot/behavior/neo_bot_grenade_throw_smoke.cpp
@@ -2,10 +2,7 @@
 #include "neo_player.h"
 #include "bot/neo_bot.h"
 #include "bot/behavior/neo_bot_grenade_throw_smoke.h"
-#include "bot/neo_bot_path_compute.h"
 #include "weapon_neobasecombatweapon.h"
-
-#include "nav_pathfind.h"
 
 CNEOBotGrenadeThrowSmoke::CNEOBotGrenadeThrowSmoke( CNEOBaseCombatWeapon *pWeapon, const CKnownEntity *threat )
 	: CNEOBotGrenadeThrow( pWeapon, threat )
@@ -29,7 +26,7 @@ CNEOBotGrenadeThrow::ThrowTargetResult CNEOBotGrenadeThrowSmoke::UpdateGrenadeTa
 	
 	if (m_vecTarget == vec3_invalid)
 	{
-		const Vector& vecThrowTarget = FindEmergencePointAlongPath(me, me->GetAbsOrigin(), m_vecThreatLastKnownPos);
+		const Vector vecThrowTarget = me->FindVisibleThrowPointNear( m_vecThreatLastKnownPos );
 
 		if (vecThrowTarget != vec3_invalid)
 		{
